7e78707f7903881e00affc3d2f333ef0894d8fe0
tcystems <systems@thinkcube.com>
2026-04-06 09:55:39 +0530

    Delete src directory

--- src/app/pages/aboutus/environment.ts
+++
@@ -1,6 +0,0 @@
-// environment.ts
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000',
-  useLocalMockApi: false, // Changed to false to use real API
-};
--- src/app/pages/event/eventflow/eventflow.interface.ts
+++
@@ -1,9 +0,0 @@
-// Interface for the article object structure
-interface Eventflow {
-  banner: string;
-  heading: string;
-  title?: string;
-  subtitle?: string;
-  contentone: string;
-}
-  
